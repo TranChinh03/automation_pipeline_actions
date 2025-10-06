357e6994aeaed3157fed94df9c259814ea08e6ba
chinh-tran-kmss <chinh.tran@uno.bank>
2025-10-07 01:09:47 +0700

    Add cmt 1

--- demo_project/lib/main.dart
+++ demo_project/lib/main.dart
@@ -39,6 +39,14 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
+      // called again, and so nothing would appear to happen.
+      // called again, and so nothing would appear to happen.
+      // called again, and so nothing would appear to happen.
+      // called again, and so nothing would appear to happen.
+      // called again, and so nothing would appear to happen.
+      // called again, and so nothing would appear to happen.
+      // called again, and so nothing would appear to happen.
+      _counter++;
       _counter++;
     });
   }
