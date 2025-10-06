1239e0e0dce0cf23d695ea8f1abc991bf11308d7
TranChinh03 <95136995+TranChinh03@users.noreply.github.com>
2025-10-07 01:05:53 +0700

    Revert "Feat/chinhmtran/new pr"

--- demo_project/lib/main.dart
+++ demo_project/lib/main.dart
@@ -7,8 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // Add cmt 1
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -41,8 +39,6 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-
-      // Add cmt 2
       _counter++;
     });
   }
